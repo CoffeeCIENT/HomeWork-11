cbc5352e2f12323703fc6a780969dce9e5d312a6
MinDa <minashipper22@mail.ru>
2026-04-25 17:13:32 +0500

    Homework fixed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+
 public class Main {
-    public static int checkYears(int year) {
+    public static int checkLeapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным.");
         } else {
@@ -10,8 +10,8 @@ public class Main {
         return year;
     }
 
-    public static int checkClientDevice(int clientOS, int clientDeviceYear) {
-        int currentYear = 2015;
+    public static void checkClientDeviceForApp(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
         if (currentYear == clientDeviceYear && clientOS == 0) {
             System.out.println("Установите стандартную версию приложения для iOS по ссылке.");
         } else if (currentYear == clientDeviceYear && clientOS == 1) {
@@ -21,10 +21,9 @@ public class Main {
         } else if (clientDeviceYear < currentYear && clientOS == 1) {
             System.out.println("Установите облегчённую версию приложения для Android по ссылке.");
         }
-        return clientOS + clientDeviceYear;
     }
 
-    public static int chekDeliveryDistance(int deliveryDistance) {
+    public static int checkDeliveryDistance(int deliveryDistance) {
         if (deliveryDistance <= 20) {
             System.out.println("Потребуется дней: 1");
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
@@ -39,10 +38,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Задание 1:");
-        checkYears(2024);
+        checkLeapYear(2024);
         System.out.println("Задание 2:");
-        checkClientDevice(1, 2013);
+        checkClientDeviceForApp(1, 2016);
         System.out.println("Задание 3:");
-        chekDeliveryDistance(95);
+        checkDeliveryDistance(95);
     }
 }
